cccf5035d7fccf1f976fb6588c1fcf975b20cb3a
gcanaledev <gabrielcanale.dev@gmail.com>
2024-04-02 21:46:50 -0300

    add remaining visual components and color them

--- app/src/main/java/com/example/age_calculator_app/MainActivity.kt
+++ app/src/main/java/com/example/age_calculator_app/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.age_calculator_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val conversionOptionsSpinner = findViewById<Spinner>(R.id.conversionOptionsSpinner)
+
+        ArrayAdapter.createFromResource(this, R.array.conversionOptions,
+            android.R.layout.simple_spinner_item).also {
+                adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            conversionOptionsSpinner.adapter = adapter
+        }
     }
 }
