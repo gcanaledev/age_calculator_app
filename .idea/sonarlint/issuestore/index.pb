65e6c08ad995b41628a1dd851c276d4cd70d24f8
gcanaledev <gabrielcanale.dev@gmail.com>
2024-04-03 22:33:33 -0300

    add calendar button functionality and initiate calculate one

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,3 @@
 
 p
 @app/src/main/java/com/example/age_calculator_app/MainActivity.kt,d\3\d319b3d2bca3dfcf9defd6a2c9ff2f6f5edc6f70
-ç
-]app/src/main/java/com/example/age_calculator_app/visualComponents/ConvertionOptionsSpinner.kt,c\c\cc7d7d4fac1849601fa6ad2116b5e187e79d4d67
-ç
-]app/src/main/java/com/example/age_calculator_app/visualComponents/ConversionOptionsSpinner.kt,2\6\2667599f5462cdbc31b4f1f5bab9b7277d02bd31
--- app/src/main/java/com/example/age_calculator_app/MainActivity.kt
+++ app/src/main/java/com/example/age_calculator_app/MainActivity.kt
@@ -1,15 +1,70 @@
 package com.example.age_calculator_app
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fillOptionsSpinner()
+        createCalendarButtonFunctionality()
+        createCalculateButtonFunctionality()
+    }
+
+    private fun createCalculateButtonFunctionality(){
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+        val optionsSpinner = findViewById<Spinner>(R.id.conversionOptionsSpinner)
+        val chosenDateTextView = findViewById<TextView>(R.id.chosenAgeTextView)
+
+        fun onCalculateButtonClicked(){
+
+            if (optionsSpinner.selectedItemPosition == 0 || chosenDateTextView.text.isNullOrBlank()){
+                Toast.makeText(this, "please, choose a valid conversion option and date",
+                    Toast.LENGTH_SHORT).show()
+
+                return
+            }
+        }
+
+        calculateButton.setOnClickListener { onCalculateButtonClicked() }
+    }
+
+    private fun createCalendarButtonFunctionality(){
+
+        fun setChosenDateTextViewText(year: Int, month: Int, dayOfMonth: Int){
+
+            val chosenDateTextView = findViewById<TextView>(R.id.chosenAgeTextView)
+            chosenDateTextView.text = String.format("%d/%d/%d", dayOfMonth, month, year)
+        }
+
+        fun openDatePicker(){
+
+            val calendar = Calendar.getInstance()
+
+            val datePickerDialog = DatePickerDialog(this,
+                { _, receivedYear, receivedMonth, receivedDayOfMonth ->  setChosenDateTextViewText(receivedYear, receivedMonth, receivedDayOfMonth)},
+                calendar[Calendar.YEAR],
+                calendar[Calendar.MONTH],
+                calendar[Calendar.DAY_OF_MONTH])
+
+            datePickerDialog.show()
+        }
+
+        val calendarButton = findViewById<Button>(R.id.calendarButton)
+
+        calendarButton.setOnClickListener{ openDatePicker() }
+    }
+
+    private fun fillOptionsSpinner(){
         val conversionOptionsSpinner = findViewById<Spinner>(R.id.conversionOptionsSpinner)
 
         ArrayAdapter.createFromResource(this, R.array.conversionOptions,
